074bf527977155527fe681688ff4c105325fed8e
dennisd1 <mbl.acc4@gmail.com>
2017-01-03 20:05:24 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,8 +4,17 @@
 # authors: Robin Ward
 # url: https://github.com/discourse/purple-tentacle
 
-add_admin_route 'purple_tentacle.title', 'purple-tentacle'
 
-Discourse::Application.routes.append do
-  get '/admin/plugins/purple-tentacle' => 'admin/plugins#index'
+# add_admin_route 'purple_tentacle.title', 'purple-tentacle'
+#
+# Discourse::Application.routes.append do
+#  get '/admin/plugins/purple-tentacle' => 'admin/plugins#index'
+#end
+
+register_asset 'javascripts/discourse/templates/user/profile.hbs'
+
+after_initialize do
+ Discourse::Application.routes.append do
+   get 'users/:username/profile' => 'users#show', constraints: {username: USERNAME_ROUTE_FORMAT}
+ end
 end
